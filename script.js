92740ff409ac3d0f13e4143d6d923811a23d68c8
herilion <heritierlionge@yahoo.com>
2022-09-02 20:42:31 +0200

    Ajout de commentaire

---
+++ script.js
@@ -0,0 +1,15 @@
+function fonctionAnagram(mot1, mot2) {
+    //cas où les mots renferment pas le même nombre de lettre
+    if (mot1.length !== mot2.length) {
+        return false;
+    }
+
+    //ici ca va Triez les deux chaînes tout en divisant la chaine en lettre qui la compose
+    var s1 = mot1.split('').sort().join('');
+    var s2 = mot2.split('').sort().join('');
+
+    return (s1 === s2);
+}
+
+console.log(fonctionAnagram('silent', 'listen'));
+console.log(fonctionAnagram('heritier', 'hertier'));
